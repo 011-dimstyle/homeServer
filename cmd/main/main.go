a14ef7f0c3e61480aecf94b3b85127fd23ee1b59
dimstyle <smilehacking27@gmail.com>
2025-12-29 00:23:17 +0700

    add templates

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,19 +1,25 @@
 package main
 
-import (
+import(
+	"html/template"
 	"net/http"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main(){
-	r := chi.NewRouter()
+	router := chi.NewRouter()
 
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("hello world !!!"))
+	router.Use(middleware.Logger)
+	router.Use(middleware.Recoverer)
+
+	fs := http.FileServer(http.Dir("./static"))
+	router.Handle("/static/*",http.StripPrefix("/static/",fs))
+
+	templates := template.Must(template.ParseGlob("templates/*.html"))
+	router.Get("/",func(w http.ResponseWriter, r *http.Request){
+		templates.ExecuteTemplate(w,"index.html", make(map[string]string))
 	})
 
-	http.ListenAndServe(":8080",r)
+	http.ListenAndServe(":8080",router)
 }
